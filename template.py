eb9ba426848a9904aade6fbb59bca57292537727
muzuvajoshua <muzuvajoshua@gmail.com>
2024-05-08 11:32:17 +0300

    folder structure

---
+++ template.py
@@ -0,0 +1,61 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "swahiliNewsClassifier"
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/prepare_base_model.py",
+    f"src/{project_name}/components/model_training.py",
+    f"src/{project_name}/components/model_evaluation.py",
+    f"src/{project_name}/utils/_init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/pipeline/stage_01_data_ingestion.py",
+    f"src/{project_name}/pipeline/stage_02_prepare_base_model.py",
+    f"src/{project_name}/pipeline/stage_03_model_training.py",
+    f"src/{project_name}/pipeline/stage_04_model_evaluation.py",
+    f"src/{project_name}/pipeline/stage_05_prediction.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "main.py",
+    "dvc.yaml",
+    "Dockerfile",
+    "logs/logfile.log",
+    "research/01_data_ingestion.ipynb",
+    "research/02_prepare_base_model.ipynb",
+    "research/03_model_training.ipynb",
+    "research/04_model_evaluation.ipynb",
+    "templates/index.html"
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory;{filedir} for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists")
